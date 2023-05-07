fb1aa4dfc71f446493ab37cf30c7e626dd2064f0
Aleksandr Belyi <aleksandr.belyiGE@outlook.com>
2023-05-07 21:17:46 +0300

    readme text created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Final work for Intro to C-Sharp*/
+
+/*Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+Примеры:
+["hello", "2", "world', ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> []
+*/
+
+
+using System;
+using static System.Console;
+
